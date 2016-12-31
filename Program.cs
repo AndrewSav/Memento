35819c5ebd0a02607bebd601e8736d8b0b76b2ed
Andrew Savinykh <github.com@brutsoft.com>
2017-01-01 00:59:44 +1300

    Added unhandled exception handler

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
-using Memento.Helpers;
+using Serilog;
 
 namespace Memento
 {
@@ -11,37 +9,19 @@ namespace Memento
         [STAThread]
         static void Main()
         {
-
-            //string path = @"D:\Projects\ConsoleApplication19\bin\Debug\Saves\Darkest Dungeon Profile 0";
-
-            //var b1 = BackupFolders.GetBackupsDescending(path).ToArray();
-
-            //List<string> x = new List<string>();
-            //string t = BackupFolders.FindNext(path);
-            //while(t != null)
-            //{
-            //    x.Add(t);
-            //    t = BackupFolders.FindNext(path,t);
-            //}
-            //var b2 = x.ToArray();
-
-            //if (b1.Length == b2.Length)
-            //{
-            //    for (int i = 0; i < b1.Length; i++)
-            //    {
-            //        if (b1[i] != b2[i])
-            //        {
-            //            Console.WriteLine(@"Bla!");
-            //        }
-            //    }
-            //}
-
-            //LoggerHelper.GetLoggerForFolder(@"d:\Andrew");
-
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Forms.Memento());
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var log = new LoggerConfiguration().WriteTo.RollingFile("unlandled.log").CreateLogger();
+            Exception ex = e.ExceptionObject as Exception;
+            log.Fatal(ex, "Unhandled exception");
+            Environment.FailFast("Unhandled exception", ex);
         }
     }
 }
